a8e65236ad7b621dabafd9aa3dab08eb599fdaef
Charles Perniciaro III <blackbeltcharles@gmail.com>
2013-03-15 21:18:12 -0500

    Added a function to move the sphre to an x, z pt.

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class Move : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
+	}
+	
+	void move(double x, double z)
+	{
+		
 	}
 }
