9286c819b35701e2d5b36e5172b508f5de5c9f1b
Charles Perniciaro III <blackbeltcharles@gmail.com>
2013-03-30 17:37:09 -0500

    Trying to get ball to move

--- Assets/LoadLevel.cs
+++ Assets/LoadLevel.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class LoadLevel : MonoBehaviour 
 {
 	// Use this for initialization
-	void Start () 
+	void Start() 
 	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update() 
 	{
 	
 	}
--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,22 +1,26 @@
 using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () 
-	{
+public class Move : MonoBehaviour 
+{
+	float speed;
+	Vector3 click;
 	
+	// Use this for initialization
+	void Start() 
+	{
+		speed = 1.0F;
+		click = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update() 
 	{
-	
-	}
-	
-	void move(double x, double z)
-	{
-		
+		if(Input.GetMouseButtonDown(0))
+		{
+			click = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			click = new Vector3(click.x, 1.0F, click.z);
+		}
+		transform.position = Vector3.Lerp(transform.position, click, speed);
 	}
 }
