ac7d03452bd1cc4220fa361e46282cdcb90e66d2
John Stolf <jstolf@uno.edu>
2013-04-22 10:16:33 -0500

    Added new Level
    Press n key to go to level 2.

--- Assets/LoadLevel.cs
+++ Assets/LoadLevel.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class LoadLevel : MonoBehaviour 
 {
+	
+	string nextLevel;
+	
 	// Use this for initialization
 	void Start() 
 	{
-	
+		nextLevel = "n";
 	}
 	
 	// Update is called once per frame
 	void Update() 
 	{
-	
+		if(Input.GetKey(nextLevel)){
+			Application.LoadLevel("Level 2");	
+		}
 	}
 }
